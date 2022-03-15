fff988434c83e97b8f4654e89809145b3d8f7796
Francesco Moles <francesco.moles@outlook.it>
2022-03-15 16:00:20 +0100

    init document

---
+++ script.js
@@ -0,0 +1,5 @@
+/*creare una griglia di gioco quadrata, in cui ogni cella contiene un 
+numero tra quelli compresi in un range compreso tra 1 e 100
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.*/
+
+
