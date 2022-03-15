91e286957b530148e878ef90c92877481b2c7dd8
Francesco Moles <francesco.moles@outlook.it>
2022-03-15 16:22:34 +0100

    grid created

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ numero tra quelli compresi in un range compreso tra 1 e 100
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.*/
 
 
+const gridBox = document.getElementById("box");
+
+const maxCells = 100;
+
+for (let i = 0; i < maxCells; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.innerText = i + 1;
+    gridBox.appendChild(cell);
+}
